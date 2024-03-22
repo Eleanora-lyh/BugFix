763237ad6bc469513afd4d142f627be934bcf2d6
Eleanora-lyh <68309092+Eleanora-lyh@users.noreply.github.com>
2024-03-22 15:45:43 +0800

    [ADD] SpirePdf生成复杂格式表格跨页时错位

---
+++ T_SDS_TEMPLATE.sql
@@ -0,0 +1,241 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : 172.75.200.17 SDS
+ Source Server Type    : Oracle
+ Source Server Version : 110200
+ Source Host           : 172.75.200.17:1521
+ Source Schema         : SDS_USER
+
+ Target Server Type    : Oracle
+ Target Server Version : 110200
+ File Encoding         : 65001
+
+ Date: 22/03/2024 15:30:31
+*/
+
+
+-- ----------------------------
+-- Table structure for T_SDS_TEMPLATE
+-- ----------------------------
+DROP TABLE "SDS_USER"."T_SDS_TEMPLATE";
+CREATE TABLE "SDS_USER"."T_SDS_TEMPLATE" (
+
+)
+LOGGING
+NOCOMPRESS
+PCTFREE 10
+INITRANS 1
+STORAGE (
+  INITIAL 65536 
+  NEXT 1048576 
+  MINEXTENTS 1
+  MAXEXTENTS 2147483645
+  BUFFER_POOL DEFAULT
+)
+PARALLEL 1
+NOCACHE
+DISABLE ROW MOVEMENT
+;
+COMMENT ON TABLE "SDS_USER"."T_SDS_TEMPLATE" IS '|{R+h嬈坔';
+
+-- ----------------------------
+-- Records of T_SDS_TEMPLATE
+-- ----------------------------
+INSERT INTO "SDS_USER"."T_SDS_TEMPLATE" VALUES ('527362835816517', 'QG-SDS-H-002', '润滑油脂', 'Optigear BM100', '齿轮油', '1111', '嘉实多（上海）管理有限公司', '《化学品注册、评估、授权和 限制制度》', '进入体内途径：皮肤或吸入', '吸入:接触分解产物下会导致健康危险。 暴露后，严重的影响会延迟才出现。
+食入:刺激口腔、咽喉和胃。
+皮肤接触:造成轻微皮肤刺激。 使皮肤脫脂。 可能造成皮肤过敏反应。', '502900374528082', 'IP346:DMSO <3%', '液体', '齿轮润滑', '如接触，请立即以大量清水冲洗皮肤至少 15 分钟并脱去污染的衣物与鞋子。 用肥皂与水彻底清洗皮肤，或使用认可的皮肤清洁剂清洗。 衣物重新使用前应清洗。鞋子在重新使用前应彻底清洗。 在任何疾病或症状存在的情况下，应避免进一步曝露。寻求医疗救护。', '如接触，请立即以大量清水冲洗眼睛至少 15 分钟。 将眼皮翻开冲洗，以确保彻底洗净。检查和取出任何隐形眼镜。 寻求医疗救护。', '如吸入，移至空气新鲜处。 在火灾时吸入分解产品后，症状可能延迟才出现。 受到暴露的患者须医疗观察 48小时。 如果出现症状，寻求医疗救护。', '禁止催吐，除非有专业医疗人士指导。 切勿给失去意识者任何口服物。 如失去知觉，应置于恢复体位并立即寻求医疗救治。 如有害的健康影响持续存在或加重，应寻求医疗救治。', '使用泡沫或多功能干粉灭火。禁止用水直接喷射。', '金属屑火灾 – 纯的金属加工油如果接触到赤热的金属屑会冒烟、热分解或点燃。为了减少赤热的金属屑的产生，应确定有足够的加工油，在切削操作时淹过工具的切割边缘。还有额外的注意就是，金属屑应当定期从工作区清除掉，以避免火灾危险。
+必须收集被本产品污染了的消防水，且禁止将其排放到任何水道（下水道或排水沟）。 在燃烧或加热情况下，会发生压力增加与容器爆裂。 本物质对水生物有害并具有长期持久影响。', '小量泄漏：若无危险，阻止泄漏。 将容器移离泄漏区域。 用惰性材料吸收并放在适当的废物处理容器中。 经由特许的废弃品处理合同商处置。大量泄漏：若无危险，阻止泄漏。 将容器移离泄漏区域。 从上风向接近泄漏物。 防止进入下水道、水道、地下室或密闭区域。 用不燃吸收剂如沙、土、蛭石、硅藻土来控制收集泄漏物，并装在容器内，以根据当地的法规要求处理。 被污染的吸附物质可呈现与溢出产品同样的危险。 经由特许的废弃品处理合同商处置。', '穿戴适当的个人防护设备 (参阅第 8部分)。 患有皮肤过敏史的个体不应受雇于任何 与本产品有关的作业。 避免接触进入眼睛、皮肤或衣物。 禁止食入。 避免吸入 蒸气或烟雾。 保持在原装容器或已批准的由相容的材料制成的代替品中，不使用时 容器保持密闭。 空容器中保留有产品残余物且可能非常危险。 请勿重复使用容 器。 避免溢出物接触土壤及流入排水沟。 密闭空间内薄雾、浓烟和蒸汽的浓缩可 能形成爆炸性空气。必须避免过度的喷溅、搅动或加热。 在金属加工过程中，工作 部件或工具中的固体颗粒会污染流体，可能会导致皮肤的磨损。这样的磨损导致皮肤 的破损时，应尽可能采取急救处理。在工作部件或工具中否些金属，例如铬、钴和 镍，可能会污染金属加工液体，以及产生细菌，结果可能导致过敏或其它皮肤反应， 尤其是在个人卫生习惯不好时。', '按照当地法规要求来储存。 储存于原装容器中，防止直接光照，置于干燥、凉爽和 通风良好的区域，远离禁忌物（见第10部分）、食品和饮料。 使用容器前，保持容 器关紧与密封。 存放或使用时仅限本产品专用的设备/容器。 已开封的容器必须小 心地再封好，并保持直立以防止漏出。 请勿储存在未加标签的容器中。 采用合适 的收容方式以防止污染环境。', '如果会长时间或反复接触，请戴防护手套。 请配戴抗化学作用的手套。 建议：腈
+胶手套。 防护手套的正确选择取决于所处理的化学药品、工作环境和使用情况、手
+套的状况 (即使最好的抗化学药品防护手套在重复接触化学药品之后也会损坏)。大多
+数的手套仅仅提供短时间的保护，然后就必须扔掉，重新更换。因为指定的工作环境
+和处理材料的不同，所以必须针对每种用途订立安全措施。因此选择手套时应该询问
+供应商/制造商，并充分评估工作条件。', '通风不良时，佩戴适当的呼吸防护器具。为保护不受金属加工液的危害，请在适当情况下选择“耐油型”（R 类）或“防油型”（P 类）呼吸防护设备。根据气载污染物的程度，可相应地选择佩戴过滤式半面罩呼吸器（带高效微粒空气过滤器），包括一次性面罩（P 类或 R 类）（适用于油雾级别低于 50 毫克/平方米的情况）、或配有头罩或头盔且带高效微粒空气过滤器的任何电动空气净化呼吸器（适用于油雾级别低于 125 毫克/平方米的情况）。金属加工操作中的有机挥发物可能带来潜在危害，因此要佩戴可过滤掉结合颗粒和有机挥发物的防护面罩呼吸系统保护装备的正确选择取决于被操作的化学药品、工作与使用条件以及呼吸装备的条件。必须为每一项预定应用编写安全规程。因此，在选择呼吸保护装备时，必须向供应商/制造商咨询，还要全面评估工作条件。', '穿着防护衣物也是一种好的行业惯例。个人防护用品的选择应以执行工作种', '戴有側罩的安全防护眼镜。', '通常应针对症状进行治疗，并且应直接减轻任何影响。在火灾时吸入分解产品后，症状可能延迟才出现。受到暴露的患者须医疗观察 48小时。', '11', '11', '<1000 公斤/m³ (<1 g/cm³) 在 15℃', '水 不可溶', '磷氧化物 金属氧化物 碳氧化物 (CO、CO2) 硫氧化物 (SO、SO2 等) 氮氧化物 (NO, NO2 等)', '与下列物质不相容或具有反应性： 氧化物质。', '本产品稳定。没有关于本产品的具体测试数据。 有关更多信息，请参阅“要避免的状况和不兼容的材料”。', '钼酸铵和C12-C24-二乙氧基烷基胺的反应产物 (1:5-1:3)
+LD50 皮肤 OECD 402 大鼠 >2000 mg/kg（毫克/千克）
+- -
+LD50 口服 OECD 401 大鼠 >2000 mg/kg（毫
+克/千克）
+- -
+顺丁烯二酸酐 LD50 皮肤 DIN 兔子 2620 mg/kg（毫
+克/千克）
+- -
+LD50 口服 OECD 401 大鼠 1090 mg/kg（毫
+克/千克）
+-', '本物质对水生物有害并具有长期持久影响。', '应尽可能避免或减少废物的产生。 显著数量的残留产品废物不应通过污水渠处置，而应在一个合适的污水处理厂内处理。 经由特许的废弃物处理合同商处理剩余物与非再生产品。 产品、溶液和其副产品的处置应符合环境保护、废弃物处理法规和当地相关法规的要求。 包装废弃物应回收。 仅在回收利用不可行时，才考虑焚烧或
+填埋。 采用安全的方法处理本品及其容器。 操作处置没有清洁或冲洗的空容器时，应小心处理。 空的容器或内衬可能保留一些产品的残余物。 避免溢出物扩散和流走，避免溢出物接触进入土壤、河流、下水道和污水管道。', '保证所有另外的当地政府运输情况符合', '针对有关产品的安全、健康和环境条例无已知的特定的国家和/或区域性法规适用于本品（包括其组分）。', '11', '珠海经济特区惠嘉有限公司', '珠海凌达', TO_TIMESTAMP_TZ('2024-03-20 14:24:54.3993589 +08:00', 'SYYYY-MM-DD HH24:MI:SS:FF7 TZR'), TO_TIMESTAMP_TZ('2024-03-21 16:29:32.9076599 +08:00', 'SYYYY-MM-DD HH24:MI:SS:FF7 TZR'), 'G590001', '珠海经济特区惠嘉有限公司', '114234136', '颜碧露', '0', 'BM100', 'BM100', '0');
+INSERT INTO "SDS_USER"."T_SDS_TEMPLATE" VALUES ('527362946633797', 'QG-SDS-H-001', '润滑油脂', 'Optigear BM100', '齿轮油', '/', '嘉实多（上海）管理有限公司', '《化学品注册、评估、授权和 限制制度》', '进入体内途径：皮肤或吸入', '吸入:接触分解产物下会导致健康危险。 暴露后，严重的影响会延迟才出现。
+食入:刺激口腔、咽喉和胃。
+皮肤接触:造成轻微皮肤刺激。 使皮肤脫脂。 可能造成皮肤过敏反应。', '502900374528082', 'IP346:DMSO <3%', '液体', '齿轮润滑', '如接触，请立即以大量清水冲洗皮肤至少 15 分钟并脱去污染的衣物与鞋子。 用肥皂与水彻底清洗皮肤，或使用认可的皮肤清洁剂清洗。 衣物重新使用前应清洗。鞋子在重新使用前应彻底清洗。 在任何疾病或症状存在的情况下，应避免进一步曝露。寻求医疗救护。', '如接触，请立即以大量清水冲洗眼睛至少 15 分钟。 将眼皮翻开冲洗，以确保彻底洗净。检查和取出任何隐形眼镜。 寻求医疗救护。', '如吸入，移至空气新鲜处。 在火灾时吸入分解产品后，症状可能延迟才出现。 受到暴露的患者须医疗观察 48小时。 如果出现症状，寻求医疗救护。', '禁止催吐，除非有专业医疗人士指导。 切勿给失去意识者任何口服物。 如失去知觉，应置于恢复体位并立即寻求医疗救治。 如有害的健康影响持续存在或加重，应寻求医疗救治。', '使用泡沫或多功能干粉灭火。禁止用水直接喷射。', '金属屑火灾 – 纯的金属加工油如果接触到赤热的金属屑会冒烟、热分解或点燃。为了减少赤热的金属屑的产生，应确定有足够的加工油，在切削操作时淹过工具的切割边缘。还有额外的注意就是，金属屑应当定期从工作区清除掉，以避免火灾危险。
+必须收集被本产品污染了的消防水，且禁止将其排放到任何水道（下水道或排水沟）。 在燃烧或加热情况下，会发生压力增加与容器爆裂。 本物质对水生物有害并具有长期持久影响。', '小量泄漏：若无危险，阻止泄漏。 将容器移离泄漏区域。 用惰性材料吸收并放在适当的废物处理容器中。 经由特许的废弃品处理合同商处置。大量泄漏：若无危险，阻止泄漏。 将容器移离泄漏区域。 从上风向接近泄漏物。 防止进入下水道、水道、地下室或密闭区域。 用不燃吸收剂如沙、土、蛭石、硅藻土来控制收集泄漏物，并装在容器内，以根据当地的法规要求处理。 被污染的吸附物质可呈现与溢出产品同样的危险。 经由特许的废弃品处理合同商处置。', '穿戴适当的个人防护设备 (参阅第 8部分)。 患有皮肤过敏史的个体不应受雇于任何 与本产品有关的作业。 避免接触进入眼睛、皮肤或衣物。 禁止食入。 避免吸入 蒸气或烟雾。 保持在原装容器或已批准的由相容的材料制成的代替品中，不使用时 容器保持密闭。 空容器中保留有产品残余物且可能非常危险。 请勿重复使用容 器。 避免溢出物接触土壤及流入排水沟。 密闭空间内薄雾、浓烟和蒸汽的浓缩可 能形成爆炸性空气。必须避免过度的喷溅、搅动或加热。 在金属加工过程中，工作 部件或工具中的固体颗粒会污染流体，可能会导致皮肤的磨损。这样的磨损导致皮肤 的破损时，应尽可能采取急救处理。在工作部件或工具中否些金属，例如铬、钴和 镍，可能会污染金属加工液体，以及产生细菌，结果可能导致过敏或其它皮肤反应， 尤其是在个人卫生习惯不好时。', '按照当地法规要求来储存。 储存于原装容器中，防止直接光照，置于干燥、凉爽和 通风良好的区域，远离禁忌物（见第10部分）、食品和饮料。 使用容器前，保持容 器关紧与密封。 存放或使用时仅限本产品专用的设备/容器。 已开封的容器必须小 心地再封好，并保持直立以防止漏出。 请勿储存在未加标签的容器中。 采用合适 的收容方式以防止污染环境。', '如果会长时间或反复接触，请戴防护手套。 请配戴抗化学作用的手套。 建议：腈胶手套。 防护手套的正确选择取决于所处理的化学药品、工作环境和使用情况、手套的状况 (即使最好的抗化学药品防护手套在重复接触化学药品之后也会损坏)。大多数的手套仅仅提供短时间的保护，然后就必须扔掉，重新更换。因为指定的工作环境和处理材料的不同，所以必须针对每种用途订立安全措施。因此选择手套时应该询问供应商/制造商，并充分评估工作条件。', '通风不良时，佩戴适当的呼吸防护器具。为保护不受金属加工液的危害，请在适当情况下选择“耐油型”（R 类）或“防油型”（P 类）呼吸防护设备。根据气载污染物的程度，可相应地选择佩戴过滤式半面罩呼吸器（带高效微粒空气过滤器），包括一次性面罩（P 类或 R 类）（适用于油雾级别低于 50 毫克/平方米的情况）、或配有头罩或头盔且带高效微粒空气过滤器的任何电动空气净化呼吸器（适用于油雾级别低于 125 毫克/平方米的情况）。金属加工操作中的有机挥发物可能带来潜在危害，因此要佩戴可过滤掉结合颗粒和有机挥发物的防护面罩呼吸系统保护装备的正确选择取决于被操作的化学药品、工作与使用条件以及呼吸装备的条件。必须为每一项预定应用编写安全规程。因此，在选择呼吸保护装备时，必须向供应商/制造商咨询，还要全面评估工作条件。', '穿着防护衣物也是一种好的行业惯例。个人防护用品的选择应以执行工作种类和所冒风险为根据，并且须得到专业人员的核准。
+棉制或聚酯/棉制工装只能针对不会渗透至皮肤的轻微表面污染提供防护。工装应定期
+清洗。如果接触到皮肤的机率较高（例如清理溢出物或存在发生喷溅的风险），则需使用抗化学作用的工作围裙和/或防渗透的套装和靴。', '戴有側罩的安全防护眼镜。', '通常应针对症状进行治疗，并且应直接减轻任何影响。在火灾时吸入分解产品后，症状可能延迟才出现。受到暴露的患者须医疗观察 48小时。', '/', '/', '<1000 公斤/m³ (<1 g/cm³) 在 15℃', '水 不可溶', '磷氧化物 金属氧化物 碳氧化物 (CO、CO2) 硫氧化物 (SO、SO2 等) 氮氧化物 (NO, NO2 等)', '与下列物质不相容或具有反应性： 氧化物质。', '本产品稳定。没有关于本产品的具体测试数据。 有关更多信息，请参阅“要避免的状况和不兼容的材料”。', '钼酸铵和C12-C24-二乙氧基烷基胺的反应产物 (1:5-1:3)
+LD50 皮肤 OECD 402 大鼠 >2000 mg/kg（毫克/千克）
+- -
+LD50 口服 OECD 401 大鼠 >2000 mg/kg（毫
+克/千克）
+- -
+顺丁烯二酸酐 LD50 皮肤 DIN 兔子 2620 mg/kg（毫
+克/千克）
+- -
+LD50 口服 OECD 401 大鼠 1090 mg/kg（毫
+克/千克）
+-', '本物质对水生物有害并具有长期持久影响。', '应尽可能避免或减少废物的产生。 显著数量的残留产品废物不应通过污水渠处置，而应在一个合适的污水处理厂内处理。 经由特许的废弃物处理合同商处理剩余物与非再生产品。 产品、溶液和其副产品的处置应符合环境保护、废弃物处理法规和当地相关法规的要求。 包装废弃物应回收。 仅在回收利用不可行时，才考虑焚烧或
+填埋。 采用安全的方法处理本品及其容器。 操作处置没有清洁或冲洗的空容器时，应小心处理。 空的容器或内衬可能保留一些产品的残余物。 避免溢出物扩散和流走，避免溢出物接触进入土壤、河流、下水道和污水管道。', '保证所有另外的当地政府运输情况符合', '针对有关产品的安全、健康和环境条例无已知的特定的国家和/或区域性法规适用于本品（包括其组分）。', '/', '珠海经济特区惠嘉有限公司', '珠海凌达', TO_TIMESTAMP_TZ('2024-03-20 14:25:21.4660951 +08:00', 'SYYYY-MM-DD HH24:MI:SS:FF7 TZR'), TO_TIMESTAMP_TZ('2024-03-21 00:28:00.7322849 +08:00', 'SYYYY-MM-DD HH24:MI:SS:FF7 TZR'), 'G590001', '珠海经济特区惠嘉有限公司', '114191466', '张贵林', '0', 'BM100', 'BM100', '2');
+INSERT INTO "SDS_USER"."T_SDS_TEMPLATE" VALUES ('526721255227461', 'QG-SDS-P-003', '伊来康', 'ELECTRA COAT AEROSOL', '橡胶质保护涂层', '5687', '安治化工有限公司', '5687', '用喷雾包装，只需对准清洁后的表面喷涂即可', '吸入危害 类别1
+急性毒性（皮肤） 类别5
+急性吸入毒性 – 气体 类别4
+急性吸入毒性 – 粉尘和烟雾 类别4
+皮肤腐蚀/刺激 类别2
+生殖毒性 类别2
+特异性靶器官系统毒性 （一次接触） 类别3
+特异性靶器官系统毒性 （反复接触） 类别2', '502900374519879', '1330-20-7:二甲苯异构体 15-40;110-54-3:正己烷 15-40;110-82-7:环己烷 15-40;9003-55-8:苯乙烯-丁二烯聚合物 10-30;74-98-6:丙烷 10-30', '无色 - 透明色', '不会滴落、松弛和干裂剥落，在设备表面形成韧性且易清除的保护性涂层，即使浸没于水中也可起到隔 绝水份的作用', '用吸附性材料擦拭（如织物、毛绒）。用肥皂和大量水清洗。如刺激发展或持续，就医。再次使用前清洗受沾染衣物。', '用大量水彻底冲洗，包括眼睑。如戴隐形 眼镜并可方便地取出，隐形。继续 冲洗 。如刺激发展或持续，就医。', '若吸入，将人转移到空气新鲜处。如呼吸短促，给氧。如停止呼吸，进行人工呼吸。立即就医。', '喝1-2杯水，禁止催吐。如刺激发展或持续，就医。', '泡沫、干粉、水喷雾、二氧化碳。根据当地情况和周围环境选用适当的灭火介质。', '气溶胶溶剂味', '个体防护措施
+穿戴防护手套/衣服。移除所有点火源。确保充分通风。在确保安全的前提下，采取措施防止进一步的泄漏或溢出。
+环境预防措施
+禁止溢出物流入地表水河及 生活污水系统。
+控制方法
+控制溢出，用不可燃的吸附材料（例如沙、泥土、硅藻土、蛭石）吸收并转移至容器中，根据当地/国家法规要求处置（见第13部分）。
+清理方法
+用洁净的无火花工具集合已吸附的材料。
+中和剂
+不适用。', '远离明火、热源或点火源。避免吸入蒸气、烟雾或气体。避免接触皮肤、眼睛和衣物。', '储存在通风良好处。防日晒。远离热源和点火源。远离儿童。', '防渗透手套', '通风不畅时，佩戴呼吸防护装置。当工人面临的浓度超过暴露限值时，必须使用合适的经认证的呼吸器', '穿戴合适的防护衣物', '带侧护的护目镜。', '穿戴防护衣服/手套。确保作业场所附近设有洗眼装置和安全淋浴器。再次使用受污染衣物前要进行清洗。', '无可用数据', '无可用数据', '>1（空气=1.0）', '无可用数据', '无可用数据', '无可用数据', '化学稳定性
+稳定。不会发生危险的聚合反应。
+避免情况
+未知。
+禁配物
+强氧化剂。
+危险分解产物
+碳氧化物。
+可能的危险反应
+正常过程没有。', '急性毒性
+组分
+LD50经口
+LD50经皮肤
+LC50吸入
+二甲苯异构体
+= 4300mg/kg 鼠
+> 1700mg/kg 兔
+= 47635mg/L鼠4h
+正己烷
+无可用数据
+= 3000mg/kg兔
+= 48000ppm鼠4h
+环己烷
+> 5000mg/kg 鼠
+> 2000mg/kg兔
+= 13.9mg/L鼠4h
+丙烷
+无可用数据
+无可用数据
+=658mg/L鼠4h', '组分
+对藻类毒性/EC50
+对鱼类毒性/LC50
+微生物毒性
+/EC50
+水溞
+Log Pow
+二甲苯异构体
+无可用数据
+13.4mg/L黑头呆鱼96h
+2.661-4.093mg/L虹鳟鱼96h
+13.5-17.3mg/L虹鳟鱼96h
+13.1-16.5mg/L蓝鳃太阳鱼96h
+19mg/L蓝鳃太阳鱼96h
+7.711-9.591mg/L蓝鳃太阳鱼96h
+23.53-29.97mg/L黑头呆鱼96h
+780mg/L鲤鱼96h
+>780mg/L鲤鱼96h
+30.26-40.75 mg/L孔雀鱼96h
+0.0084 mg/L
+24h
+EC50=3.82 mg/L 48h
+LC50=0.6 mg/L 48h
+2.77-3.15
+正己烷
+无可用数据
+2.1-2.98 mg/L黑头呆鱼96h
+无可用数据
+EC50> 1000mg/L 24h
+不适用
+环己烷
+>500mg/L
+近具刺链带藻72h
+3.96 - 5.18mg/L黑头呆鱼96h
+23.03-42.07mg/L黑头呆鱼96h
+24.99-44.69mg/L蓝鳃太阳鱼96h
+48.87-68.76mg/L孔雀鱼96h
+85.5mg/L5min
+93mg/L 10min
+EC50> 400mg/L 24h
+3.44', '产品废弃
+按照当地法规进行废弃。
+容器废弃
+内容物处于压力下。不要刺破。清空剩余物。空容器可用于回收、再利用或废物处置。', 'UN-No
+UN1950
+运输名称
+气雾剂
+危险等级
+2.1
+运输描述
+UN1950，气雾剂，2.1', 'TSCA符合  DSL符合
+美国联邦法规
+SARA 313
+1986 年超级基金修正案及再授权法(SARA)第三篇第313 节。此产品含有需按照该法案和联邦法规第40篇第372 部分的要求向有关部门报告的化学品。
+中国
+危险化学品安全管理条例 危险化学品安全管理条例 危险化学品安全管理条例 危险化学品安全管理条例 (国务院第 国务院第 591 号令 )、工作场所安全使用化学品规定 、工作场所安全使用化学品规定 、工作场所安全使用化学品规定 、工作场所安全使用化学品规定 、工作场所安全使用化学品规定 、工作场所安全使用化学品规定 ([1996] ([1996]([1996]([1996]([1996]劳动部发 423 号)、联合国《关于危险货 、联合国《关于危险货 、联合国《关于危险货 、联合国《关于危险货 物运输的建议书》等法规，针对化学品安全使用、生产储存装卸方面均作了相应定。根据《分类和 物运输的建议书》等法规，针对化学品安全使用、生产储存装卸方面均作了相应定。根据《分类和 物运输的建议书》等法规，针对化学品安全使用、生产储存装卸方面均作了相应定。根据《分类和 物运输的建议书》等法规，针对化学品安全使用、生产储存装卸方面均作了相应定。根据《分类和 物运输的建议书》等法规，针对化学品安全使用、生产储存装卸方面均作了相应定。根据《分类和 物运输的建议书》等法规，针对化学品安全使用、生产储存装卸方面均作了相应定。根据《分类和 物运输的建议书》等法规，针对化学品安全使用、生产储存装卸方面均作了相应定。根据《分类和 物运输的建议书》等法规，针对化学品安全使用、生产储存装卸方面均作了相应定。根据《分类和 物运输的建议书》等法规，针对化学品安全使用、生产储存装卸方面均作了相应定。根据《分类和 物运输的建议书》等法规，针对化学品安全使用、生产储存装卸方面均作了相应定。根据《分类和 物运输的建议书》等法规，针对化学品安全使用、生产储存装卸方面均作了相应定。根据《分类和 物运输的建议书》等法规，针对化学品安全使用、生产储存装卸方面均作了相应定。根据《分类和 危险性公示 - 通则》（ 通则》（ GB13690GB13690 GB13690GB13690GB13690GB13690-2009 ）、危险货物品名表（ ）、危险货物品名表（ GB 12268GB 12268 GB 12268GB 12268GB 12268GB 12268GB 12268-2012201220122012）、危险货物分类和品名编号（ ）、危险货物分类和品名编号（ GB6944GB6944 GB6944GB6944GB6944-2012201220122012）、 危险化学品名录、工作场所有害因素职业接触限值 第1部分 :化学有害因素（ GBZ 2.1GBZ 2.1 GBZ 2.1GBZ 2.1 -2007200720072007）、《化学品分类和标签规范》（ ）、《化学品分类和标签规范》（ ）、《化学品分类和标签规范》（ ）、《化学品分类和标签规范》（ ）、《化学品分类和标签规范》（ GB GB 30000.230000.230000.230000.230000.230000.2 ～29 ）等标准对本品进 ）等标准对本品进 ）等标准对本品进 行分类和辨识。本 行分类和辨识。本 SDS 按照《化学品安全技术说明书编写指南》 按照《化学品安全技术说明书编写指南》 (GB/T 17519 (GB/T 17519 (GB/T 17519(GB/T 17519(GB/T 17519(GB/T 17519(GB/T 17519(GB/T 17519-2013) 2013) 2013) 2013) 2013) 2013) 以及《化 以及《化 以及《化 学品安全技术说明书 内', 'NCH China
+NFPA 美国消防协会 ACGIH TLV 美国政府工业卫生学家委员会 阈限值
+HMIS 有害物质识别系统 OSHA PEL 美国职业安全与健康 允许暴露限制
+NIOSH 美国国家职业安全卫生研究所 TWA 时间加权平均值
+TSCA 美国有毒物质控制法 DSL 加拿大国内物质清单', '安治化工有限公司', '珠海凌达', TO_TIMESTAMP_TZ('2024-03-18 18:54:18.5185752 +08:00', 'SYYYY-MM-DD HH24:MI:SS:FF7 TZR'), TO_TIMESTAMP_TZ('2024-03-19 09:51:35.9240061 +08:00', 'SYYYY-MM-DD HH24:MI:SS:FF7 TZR'), '001001', '�Ͼ�������ɫ�������޹�˾', '114191466', '张贵林', '0', '312克*12罐', '12016427', '1');
+
+-- ----------------------------
+-- Primary Key structure for table T_SDS_TEMPLATE
+-- ----------------------------
+ALTER TABLE "SDS_USER"."T_SDS_TEMPLATE" ADD CONSTRAINT "PK_T_SDS_TEMPLATE" PRIMARY KEY ("ID");
+
+-- ----------------------------
+-- Checks structure for table T_SDS_TEMPLATE
+-- ----------------------------
+ALTER TABLE "SDS_USER"."T_SDS_TEMPLATE" ADD CONSTRAINT "SYS_C00219460" CHECK ("ID" IS NOT NULL) NOT DEFERRABLE INITIALLY IMMEDIATE NORELY VALIDATE;
+ALTER TABLE "SDS_USER"."T_SDS_TEMPLATE" ADD CONSTRAINT "SYS_C00219461" CHECK ("CHINESE_NAME" IS NOT NULL) NOT DEFERRABLE INITIALLY IMMEDIATE NORELY VALIDATE;
+ALTER TABLE "SDS_USER"."T_SDS_TEMPLATE" ADD CONSTRAINT "SYS_C00219462" CHECK ("ENGLISH_NAME" IS NOT NULL) NOT DEFERRABLE INITIALLY IMMEDIATE NORELY VALIDATE;
+ALTER TABLE "SDS_USER"."T_SDS_TEMPLATE" ADD CONSTRAINT "SYS_C00219463" CHECK ("VULGO" IS NOT NULL) NOT DEFERRABLE INITIALLY IMMEDIATE NORELY VALIDATE;
+ALTER TABLE "SDS_USER"."T_SDS_TEMPLATE" ADD CONSTRAINT "SYS_C00219464" CHECK ("CHEMICAL_FORMULA" IS NOT NULL) NOT DEFERRABLE INITIALLY IMMEDIATE NORELY VALIDATE;
+ALTER TABLE "SDS_USER"."T_SDS_TEMPLATE" ADD CONSTRAINT "SYS_C00219465" CHECK ("DATA_SOURCE" IS NOT NULL) NOT DEFERRABLE INITIALLY IMMEDIATE NORELY VALIDATE;
+ALTER TABLE "SDS_USER"."T_SDS_TEMPLATE" ADD CONSTRAINT "SYS_C00219466" CHECK ("REFERENCE_STANDARD" IS NOT NULL) NOT DEFERRABLE INITIALLY IMMEDIATE NORELY VALIDATE;
+ALTER TABLE "SDS_USER"."T_SDS_TEMPLATE" ADD CONSTRAINT "SYS_C00219467" CHECK ("INVASION_ROUTE" IS NOT NULL) NOT DEFERRABLE INITIALLY IMMEDIATE NORELY VALIDATE;
+ALTER TABLE "SDS_USER"."T_SDS_TEMPLATE" ADD CONSTRAINT "SYS_C00219468" CHECK ("CATEGORY_IDENTITY_ID" IS NOT NULL) NOT DEFERRABLE INITIALLY IMMEDIATE NORELY VALIDATE;
+ALTER TABLE "SDS_USER"."T_SDS_TEMPLATE" ADD CONSTRAINT "SYS_C00219469" CHECK ("MAIN_INGREDIENT" IS NOT NULL) NOT DEFERRABLE INITIALLY IMMEDIATE NORELY VALIDATE;
+ALTER TABLE "SDS_USER"."T_SDS_TEMPLATE" ADD CONSTRAINT "SYS_C00219470" CHECK ("APPEARANCE_SHAPE" IS NOT NULL) NOT DEFERRABLE INITIALLY IMMEDIATE NORELY VALIDATE;
+ALTER TABLE "SDS_USER"."T_SDS_TEMPLATE" ADD CONSTRAINT "SYS_C00219471" CHECK ("MAIN_USAGE" IS NOT NULL) NOT DEFERRABLE INITIALLY IMMEDIATE NORELY VALIDATE;
+ALTER TABLE "SDS_USER"."T_SDS_TEMPLATE" ADD CONSTRAINT "SYS_C00219472" CHECK ("SKIN_CONTACT" IS NOT NULL) NOT DEFERRABLE INITIALLY IMMEDIATE NORELY VALIDATE;
+ALTER TABLE "SDS_USER"."T_SDS_TEMPLATE" ADD CONSTRAINT "SYS_C00219473" CHECK ("EYE_CONTACT" IS NOT NULL) NOT DEFERRABLE INITIALLY IMMEDIATE NORELY VALIDATE;
+ALTER TABLE "SDS_USER"."T_SDS_TEMPLATE" ADD CONSTRAINT "SYS_C00219474" CHECK ("INHALATION" IS NOT NULL) NOT DEFERRABLE INITIALLY IMMEDIATE NORELY VALIDATE;
+ALTER TABLE "SDS_USER"."T_SDS_TEMPLATE" ADD CONSTRAINT "SYS_C00219475" CHECK ("INGESTION" IS NOT NULL) NOT DEFERRABLE INITIALLY IMMEDIATE NORELY VALIDATE;
+ALTER TABLE "SDS_USER"."T_SDS_TEMPLATE" ADD CONSTRAINT "SYS_C00219476" CHECK ("FIRE_EXTINGUISH" IS NOT NULL) NOT DEFERRABLE INITIALLY IMMEDIATE NORELY VALIDATE;
+ALTER TABLE "SDS_USER"."T_SDS_TEMPLATE" ADD CONSTRAINT "SYS_C00219477" CHECK ("HAZARDOUS_PROPERTY" IS NOT NULL) NOT DEFERRABLE INITIALLY IMMEDIATE NORELY VALIDATE;
+ALTER TABLE "SDS_USER"."T_SDS_TEMPLATE" ADD CONSTRAINT "SYS_C00219478" CHECK ("EMERGENCY_TREATMENTT" IS NOT NULL) NOT DEFERRABLE INITIALLY IMMEDIATE NORELY VALIDATE;
+ALTER TABLE "SDS_USER"."T_SDS_TEMPLATE" ADD CONSTRAINT "SYS_C00219479" CHECK ("OPERATION_CAUTION" IS NOT NULL) NOT DEFERRABLE INITIALLY IMMEDIATE NORELY VALIDATE;
+ALTER TABLE "SDS_USER"."T_SDS_TEMPLATE" ADD CONSTRAINT "SYS_C00219480" CHECK ("TRANSPORT_CAUTION" IS NOT NULL) NOT DEFERRABLE INITIALLY IMMEDIATE NORELY VALIDATE;
+ALTER TABLE "SDS_USER"."T_SDS_TEMPLATE" ADD CONSTRAINT "SYS_C00219481" CHECK ("HAND_PROTECTION" IS NOT NULL) NOT DEFERRABLE INITIALLY IMMEDIATE NORELY VALIDATE;
+ALTER TABLE "SDS_USER"."T_SDS_TEMPLATE" ADD CONSTRAINT "SYS_C00219482" CHECK ("BREATHING_PROTECTION" IS NOT NULL) NOT DEFERRABLE INITIALLY IMMEDIATE NORELY VALIDATE;
+ALTER TABLE "SDS_USER"."T_SDS_TEMPLATE" ADD CONSTRAINT "SYS_C00219483" CHECK ("BODY_PROTECTION" IS NOT NULL) NOT DEFERRABLE INITIALLY IMMEDIATE NORELY VALIDATE;
+ALTER TABLE "SDS_USER"."T_SDS_TEMPLATE" ADD CONSTRAINT "SYS_C00219484" CHECK ("EYEWEAR_PROTECTION" IS NOT NULL) NOT DEFERRABLE INITIALLY IMMEDIATE NORELY VALIDATE;
+ALTER TABLE "SDS_USER"."T_SDS_TEMPLATE" ADD CONSTRAINT "SYS_C00219485" CHECK ("OTHER_PROTECTION" IS NOT NULL) NOT DEFERRABLE INITIALLY IMMEDIATE NORELY VALIDATE;
+ALTER TABLE "SDS_USER"."T_SDS_TEMPLATE" ADD CONSTRAINT "SYS_C00219486" CHECK ("MELTING_POINT" IS NOT NULL) NOT DEFERRABLE INITIALLY IMMEDIATE NORELY VALIDATE;
+ALTER TABLE "SDS_USER"."T_SDS_TEMPLATE" ADD CONSTRAINT "SYS_C00219487" CHECK ("BOILING_POINT" IS NOT NULL) NOT DEFERRABLE INITIALLY IMMEDIATE NORELY VALIDATE;
+ALTER TABLE "SDS_USER"."T_SDS_TEMPLATE" ADD CONSTRAINT "SYS_C00219488" CHECK ("RELATIVE_DENSITY" IS NOT NULL) NOT DEFERRABLE INITIALLY IMMEDIATE NORELY VALIDATE;
+ALTER TABLE "SDS_USER"."T_SDS_TEMPLATE" ADD CONSTRAINT "SYS_C00219489" CHECK ("RESOLVABLE" IS NOT NULL) NOT DEFERRABLE INITIALLY IMMEDIATE NORELY VALIDATE;
+ALTER TABLE "SDS_USER"."T_SDS_TEMPLATE" ADD CONSTRAINT "SYS_C00219490" CHECK ("COMBUSTION_PRODUCT" IS NOT NULL) NOT DEFERRABLE INITIALLY IMMEDIATE NORELY VALIDATE;
+ALTER TABLE "SDS_USER"."T_SDS_TEMPLATE" ADD CONSTRAINT "SYS_C00219491" CHECK ("TABOOS" IS NOT NULL) NOT DEFERRABLE INITIALLY IMMEDIATE NORELY VALIDATE;
+ALTER TABLE "SDS_USER"."T_SDS_TEMPLATE" ADD CONSTRAINT "SYS_C00219492" CHECK ("STABILITY_REACTIVITY" IS NOT NULL) NOT DEFERRABLE INITIALLY IMMEDIATE NORELY VALIDATE;
+ALTER TABLE "SDS_USER"."T_SDS_TEMPLATE" ADD CONSTRAINT "SYS_C00219493" CHECK ("TOXICOLOGICAL_INFO" IS NOT NULL) NOT DEFERRABLE INITIALLY IMMEDIATE NORELY VALIDATE;
+ALTER TABLE "SDS_USER"."T_SDS_TEMPLATE" ADD CONSTRAINT "SYS_C00219494" CHECK ("ECOLOGICAL_INFO" IS NOT NULL) NOT DEFERRABLE INITIALLY IMMEDIATE NORELY VALIDATE;
+ALTER TABLE "SDS_USER"."T_SDS_TEMPLATE" ADD CONSTRAINT "SYS_C00219495" CHECK ("DISPOSAL" IS NOT NULL) NOT DEFERRABLE INITIALLY IMMEDIATE NORELY VALIDATE;
+ALTER TABLE "SDS_USER"."T_SDS_TEMPLATE" ADD CONSTRAINT "SYS_C00219496" CHECK ("TRANSPORT_INFO" IS NOT NULL) NOT DEFERRABLE INITIALLY IMMEDIATE NORELY VALIDATE;
+ALTER TABLE "SDS_USER"."T_SDS_TEMPLATE" ADD CONSTRAINT "SYS_C00219497" CHECK ("REGULATORY_INFO" IS NOT NULL) NOT DEFERRABLE INITIALLY IMMEDIATE NORELY VALIDATE;
+ALTER TABLE "SDS_USER"."T_SDS_TEMPLATE" ADD CONSTRAINT "SYS_C00219498" CHECK ("OTHER_INFO" IS NOT NULL) NOT DEFERRABLE INITIALLY IMMEDIATE NORELY VALIDATE;
+ALTER TABLE "SDS_USER"."T_SDS_TEMPLATE" ADD CONSTRAINT "SYS_C00219499" CHECK ("MANUFACTURER" IS NOT NULL) NOT DEFERRABLE INITIALLY IMMEDIATE NORELY VALIDATE;
+ALTER TABLE "SDS_USER"."T_SDS_TEMPLATE" ADD CONSTRAINT "SYS_C00219500" CHECK ("USAGE_DEPARTMENT" IS NOT NULL) NOT DEFERRABLE INITIALLY IMMEDIATE NORELY VALIDATE;
+ALTER TABLE "SDS_USER"."T_SDS_TEMPLATE" ADD CONSTRAINT "SYS_C00219501" CHECK ("IS_DELETED" IS NOT NULL) NOT DEFERRABLE INITIALLY IMMEDIATE NORELY VALIDATE;
+ALTER TABLE "SDS_USER"."T_SDS_TEMPLATE" ADD CONSTRAINT "SYS_C00219608" CHECK ("VERSION" IS NOT NULL) NOT DEFERRABLE INITIALLY IMMEDIATE NORELY VALIDATE;
+ALTER TABLE "SDS_USER"."T_SDS_TEMPLATE" ADD CONSTRAINT "SYS_C00219877" CHECK ("IS_PRODUCTION_ITEM" IS NOT NULL) NOT DEFERRABLE INITIALLY IMMEDIATE NORELY VALIDATE;
